536779a47accd4f7b52a3435c307b3e1e2b44e1e
jpvalery <jpvalery@users.noreply.github.com>
2021-02-26 21:19:38 -0500

    fix: mobile fixes

--- components/Logos.js
+++ components/Logos.js
@@ -8,7 +8,7 @@ export default function Logos() {
       <h2 className="text-3xl font-extrabold text-white text-center pb-8">
         Over a decade of experience at companies like
       </h2>
-      <div className="grid grid-flow-col justify-center items-center gap-12">
+      <div className="grid grid-flow-row md:grid-flow-col justify-center items-center gap-12">
         <Gameloft />
         <LANDR />
         <LocalLogic />
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Questions from "../components/Questions";
 
 export default function Home() {
   return (
-    <div className="mx-auto py-24 grid gap-40">
+    <div className="mx-auto py-24 grid gap-20 md:gap-40">
       <h1 className="font-serif font-bold text-5xl md:text-6xl text-center py-8">
         Sustainable growth for<br />early-stage SaaS
       </h1>
