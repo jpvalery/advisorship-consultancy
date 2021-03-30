af9b0c826c50a0b431a5d00c83e52ebd609f1f89
jpvalery <jpvalery@users.noreply.github.com>
2021-03-30 09:23:35 -0400

    fix: wrong icon

--- components/Questions.js
+++ components/Questions.js
@@ -1,5 +1,5 @@
 import NextLink from "next/link";
-import { ArrowNarrowLeftIcon, InboxInIcon } from "@heroicons/react/outline";
+import { ArrowNarrowRightIcon, InboxInIcon } from "@heroicons/react/outline";
 
 export default function Questions() {
   return (
@@ -16,7 +16,7 @@ export default function Questions() {
           <NextLink href="/FAQ">
             <button className="flex items-center justify-between w-full px-4 py-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-cta-600 hover:bg-cta-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cta-500">
               Read the FAQ
-              <ArrowNarrowLeftIcon className="w-5 h-5 ml-3 -mr-1" />
+              <ArrowNarrowRightIcon className="w-5 h-5 ml-3 -mr-1" />
             </button>
           </NextLink>
           <a href="https://contact.jpvalery.me">
